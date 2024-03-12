e4adb38bd8c2363de2cffbcbcc13393e6822d86c
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-12 21:29:42 +0500

    NamesList
    New app

--- NamesList/ContentView.swift
+++ NamesList/ContentView.swift
@@ -4,18 +4,16 @@
 //
 //  Created by Насрулло Исмоилжонов on 12/03/24.
 //
-
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Data?
+    @State private var imagesList = [Data?]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("New app")
     }
 }
 
