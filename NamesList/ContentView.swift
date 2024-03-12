0ec688adc63b3a95a72d2da73bdad0ad18dcb187
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-12 20:27:05 +0500

    Initial Commit

---
+++ NamesList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NamesList
+//
+//  Created by Насрулло Исмоилжонов on 12/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NamesList/NamesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamesListApp.swift
+//  NamesList
+//
+//  Created by Насрулло Исмоилжонов on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct NamesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
